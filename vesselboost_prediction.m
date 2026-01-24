97cd55808ab02e8ccbc17c54aa4384fc5d3fb6b9
Sebastien Proulx <proulxs@stanford.edu>
2026-01-23 21:33:39 -0800

    trying vessel boost on upsampled data (computationally intensive but could save a lot of trouble later)

--- vesselboost_prediction.m
+++ vesselboost_prediction.m
@@ -1,4 +1,4 @@
-function vesselboost_prediction(ds_path, out_path, pretrained, prep_mode)
+function vesselboost_prediction(ds_path, out_path, ps_path, pretrained, prep_mode)
     global src;
     if nargin < 4 || isempty(prep_mode) ; prep_mode  = 4; end
     if nargin < 3 || isempty(pretrained); pretrained = fullfile(fileparts(ds_path),'models','manual_0429'); end
@@ -7,6 +7,7 @@ function vesselboost_prediction(ds_path, out_path, pretrained, prep_mode)
     cmd{end+1} = 'prediction.py \';
     cmd{end+1} = ['--ds_path ' ds_path ' \'];
     cmd{end+1} = ['--out_path ' out_path ' \'];
+    cmd{end+1} = ['--ps_path ' ps_path ' \'];
     cmd{end+1} = ['--pretrained ' pretrained ' \'];
     cmd{end+1} = ['--prep_mode ' num2str(prep_mode)];
     system(strjoin(cmd,newline),'-echo');
